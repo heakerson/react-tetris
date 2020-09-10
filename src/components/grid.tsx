5a4e1bc121c1b28a9971532a0548f6315eaed969
Heather Akerson <HeatherA@magenic.com>
2020-09-10 15:12:11 -0500

    grid performance improvements, start of touch functionality

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -5,11 +5,12 @@ import _ from "lodash";
 import Cell from "./cell";
 import { fromEvent } from "rxjs";
 import { Subject } from "rxjs/internal/Subject";
-import { takeUntil, filter, throttleTime } from "rxjs/operators";
+import { takeUntil, filter, throttleTime, debounceTime } from "rxjs/operators";
 import { MoveActiveShape } from "../services/store/actions";
 import { MoveDirection } from "../models/move-direction";
+import { InputType } from "../models/input-type";
 
-function Grid(props: { game: Game }) {
+const Grid = React.memo((props: { game: Game }) => {
   const takeUntil$ = new Subject();
 
   const stateData = props.game.setComponentGameStateListener(gameState => {
@@ -17,36 +18,51 @@ function Grid(props: { game: Game }) {
       gridWidth: gameState.gridWidth,
       gridHeight: gameState.gridHeight,
       grid: gameState.grid,
-      keyboardInputKeys: gameState.keyboardInputKeys
+      keyboardInputKeys: gameState.keyboardInputKeys,
+      inputType: gameState.inputType,
     };
   });
 
   useEffect(() => {
-    fromEvent(window, 'keydown')
-    .pipe(
-      takeUntil(takeUntil$),
-      // throttleTime(150),
-      filter((event: any) => Object.values(stateData.keyboardInputKeys).includes(event.key))
-    )
-    .subscribe(event => {
-      const keyboardKeys = stateData.keyboardInputKeys;
-
-      switch(event.key) {
-        case keyboardKeys.downKey:
-          moveShape(MoveDirection.Down);
-          break;
-        case keyboardKeys.rotateKey:
-          break;
-        case keyboardKeys.leftKey:
-          moveShape(MoveDirection.Left);
-          break;
-        case keyboardKeys.rightKey:
-          moveShape(MoveDirection.Right);
-          break;
-        case keyboardKeys.moveToBottomKey:
-          break;
-      }
-    });
+    if (stateData.inputType === InputType.Keyboard) {
+      fromEvent(window, 'keydown')
+        .pipe(
+          takeUntil(takeUntil$),
+          // debounceTime(50),
+          // throttleTime(150),
+          filter((event: any) => Object.values(stateData.keyboardInputKeys).includes(event.key))
+        )
+        .subscribe(event => {
+          const keyboardKeys = stateData.keyboardInputKeys;
+    
+          switch(event.key) {
+            case keyboardKeys.downKey:
+              moveShape(MoveDirection.Down);
+              break;
+            case keyboardKeys.rotateKey:
+              break;
+            case keyboardKeys.leftKey:
+              moveShape(MoveDirection.Left);
+              break;
+            case keyboardKeys.rightKey:
+              moveShape(MoveDirection.Right);
+              break;
+            case keyboardKeys.moveToBottomKey:
+              break;
+          }
+        });
+    } else {
+      alert('setting touch event!');
+      fromEvent(window, 'touchstart')
+        .pipe(
+          takeUntil(takeUntil$),
+          // throttleTime(150),
+          // filter((event: any) => Object.values(stateData.keyboardInputKeys).includes(event.key))
+        )
+        .subscribe(event => {
+        
+        });
+    }
 
     return () => { takeUntil$.next(); takeUntil$.complete(); }
   });
@@ -56,7 +72,7 @@ function Grid(props: { game: Game }) {
 
   const buildRow = (rowIndex: number) => {
     return (
-      <div className="flex-row" key={`row-${rowIndex}`}>
+      <div id='grid' className="flex-row" key={`row-${rowIndex}`}>
         {columnCounter.map(colIndex => <Cell key={`col-${colIndex}-row-${rowIndex}`} game={props.game} rowIndex={rowIndex} columnIndex={colIndex} />)}
       </div>
     );
@@ -83,11 +99,11 @@ function Grid(props: { game: Game }) {
 
   return (
     <div className="flex-column">
-      <div className="top-bar"></div>
+      <div className="top-bar shadow"></div>
       {rowCounter.map(rowIndex => buildRow(rowIndex))}
-      <div className="bottom-bar"></div>
+      <div className="bottom-bar shadow"></div>
     </div>
   );
-}
+});
 
 export default Grid;

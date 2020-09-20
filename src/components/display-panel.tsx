036a116bc7aa227b719dcdf201a80a7a1e2c57c9
Heather Akerson <HeatherA@magenic.com>
2020-09-20 10:55:49 -0500

    started adding display panel buttons

--- src/components/display-panel.tsx
+++ src/components/display-panel.tsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import Game from "../services/game";
 import NextShapeDisplay from "./next-shape-display";
 import './display-panel.css';
+import { GameStatus } from "../models/game-status";
 
 function DisplayPanel(props: { game: Game }) {
-  const scoreboardData = props.game.setComponentGameStateListener(state => {
+  const displayData = props.game.setComponentGameStateListener(state => {
     return {
       level: state.currentLevel,
       score: state.score,
       rowsCleared: state.rowsCleared,
+      gameStatus: state.gameStatus
     };
   });
 
@@ -19,15 +21,27 @@ function DisplayPanel(props: { game: Game }) {
 
   let levelUpdated = false;
 
-  if (levelData.previousLevel !== scoreboardData.level && scoreboardData.level) {
+  if (levelData.previousLevel !== displayData.level && displayData.level) {
     levelUpdated = true;
 
     setTimeout(() => {
       setlevelData({
-        previousLevel: scoreboardData.level,
+        previousLevel: displayData.level,
         updated: false
       });
     }, 3000);
+  }
+
+  const getMainButtonText = (gameStatus: GameStatus) => {
+    switch (gameStatus) {
+      case GameStatus.Start:
+        return 'START';
+      case GameStatus.Paused:
+        return 'CONTINUE';
+      default:
+        return 'PAUSE';
+
+    }
   }
 
   const levelLabelClasses = `glow-text-white stat-spacer ${levelUpdated ? 'glow-text-green-animation' : ''}`;
@@ -37,11 +51,13 @@ function DisplayPanel(props: { game: Game }) {
     <div className="content-container-fill-parent scoreboard-container inset-shadow flex-column">
       <NextShapeDisplay game={props.game} />
       <div className={levelLabelClasses}>LEVEL: </div>
-      <div className={levelClasses}>{scoreboardData.level + 1}</div>
+      <div className={levelClasses}>{displayData.level + 1}</div>
       <div className="glow-text-white stat-spacer">SCORE: </div>
-      <div className="data glow-text-fuschia">{scoreboardData.score}</div>
+      <div className="data glow-text-fuschia">{displayData.score}</div>
       <div className="glow-text-white stat-spacer">ROWS CLEARED: </div>
-      <div className="data glow-text-fuschia">{scoreboardData.rowsCleared}</div>
+      <div className="data glow-text-fuschia">{displayData.rowsCleared}</div>
+      <button disabled={displayData.gameStatus === GameStatus.End} className="glow-border-green display-panel-button hover-cursor bg-green"><span className="glow-text-green">{getMainButtonText(displayData.gameStatus)}</span></button>
+      <button className="glow-border-fuschia display-panel-button hover-cursor bg-fuschia"><span className="glow-text-fuschia">RESET</span></button>
     </div>
   );
 }
